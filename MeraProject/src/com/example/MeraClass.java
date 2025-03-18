7a182b485e4708840ef1c20dc006e303adbc37ff
Amarjeet <amarjeetmistri41@gmail.com>
2025-03-18 16:12:03 +0530

    Add More Files

---
+++ MeraProject/src/com/example/MeraClass.java
@@ -0,0 +1,30 @@
+package com.example;
+
+
+
+class Test{
+	private int a =10;
+	private void sada() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
+class Test2{
+	private void hello() {
+		System.out.println(a);
+	}
+	sda();
+}
+
+
+public class MeraClass {
+
+	public static void main(String[] args) {
+		
+		Test t  = new Test();
+		
+		
+	}
+
+}
