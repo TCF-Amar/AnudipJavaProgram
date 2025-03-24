b13201602a74a33013d2be20023c43199e39d01d
Amarjeet <amarjeetmistri41@gmail.com>
2025-03-24 16:08:18 +0530

    Add more files

---
+++ MeraProject/src/com/example/March19.java
@@ -0,0 +1,27 @@
+package com.example;
+
+class test{
+	
+}
+
+
+public class March19 {
+
+	
+	
+	
+	public static void main(String[] args) {
+		March19 obj = new March19();
+		String a = "fd";
+		System.out.println(obj.toString());
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	
+
+	
+	
+	
+}
---
+++ MeraProject/src/com/example/March21.java
@@ -0,0 +1,34 @@
+package com.example;
+
+class StrigBufferConcept{
+	
+	
+	public void display() {
+		String s1 = "Hello";
+		System.out.println(s1.hashCode());
+		 s1 = s1+"World";
+		System.out.println(s1.hashCode());
+		
+		s1 =s1.concat("he");
+	System.out.println(s1);
+	}
+	
+	
+}
+
+
+
+public class March21 {
+//1. string is immutable 
+//2. SCP(String Constant Pool)
+	public static void main(String[] args) {
+		StrigBufferConcept s = new StrigBufferConcept();
+		s.display();
+//		char a = 65;
+		
+		System.out.println(a);
+		
+
+	}
+
+}
